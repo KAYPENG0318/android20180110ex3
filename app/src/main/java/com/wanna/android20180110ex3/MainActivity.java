5ec348bef9b7fcd77bf65b50b4fcefe58cde0ee9
kay <ruby50333@yahoo.com.tw>
2018-01-11 16:01:43 +0800

    add

--- app/src/main/java/com/wanna/android20180110ex3/MainActivity.java
+++ app/src/main/java/com/wanna/android20180110ex3/MainActivity.java
@@ -32,8 +32,9 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
 public class MainActivity extends AppCompatActivity {
+    //使用https://www.mobile01.com/rss/news.xml
     ListView lv;
-    ArrayAdapter<String> adapter;
+    MyAdapter adapter;
     MyHandler dataHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,13 +105,9 @@ public class MainActivity extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    String data[] = new String[dataHandler.newsItems.size()];
-                                    for(int i = 0;i<data.length;i++)
-                                    {
-                                        data[i] = dataHandler.newsItems.get(i).title;
-                                    }
-                                    adapter = new ArrayAdapter<String>(MainActivity.this,
-                                            android.R.layout.simple_list_item_1,data);
+
+                                    adapter = new MyAdapter(MainActivity.this,
+                                            dataHandler.newsItems);
                                     lv.setAdapter(adapter);
                                 }
                             });
--- app/src/main/java/com/wanna/android20180110ex3/MyHandler.java
+++ app/src/main/java/com/wanna/android20180110ex3/MyHandler.java
@@ -7,6 +7,8 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Student on 2018/1/10.
@@ -16,42 +18,78 @@ public class MyHandler extends DefaultHandler {
     boolean isTitle = false;
     boolean isItem=false;
     boolean isLink = false;
+    boolean isDescription = false;
     StringBuilder linkSB = new StringBuilder();
+    StringBuilder descSB = new StringBuilder();
     public ArrayList<Mobile01NewsItem> newsItems = new ArrayList<>();
     Mobile01NewsItem item;
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
         super.startElement(uri, localName, qName, attributes);
-        if(qName.equals("title")){
-            isTitle = true;
+        switch(qName)
+        {
+            case "title":
+                isTitle = true;
+                break;
+            case "item":
+                isItem=true;
+                item = new Mobile01NewsItem();
+                break;
+            case "link":
+                isLink = true;
+                break;
+            case "description":
+                isDescription = true;
+                descSB = new StringBuilder();
+                break;
+
         }
-        if(qName.equals("item")){
-            isItem=true;
-            item = new Mobile01NewsItem();
-        }
-        if(qName.equals("link")){
-            isLink = true;
-        }
+
+
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
         super.endElement(uri, localName, qName);
-        if(qName.equals("title")){
-            isTitle = false;
-        }
-        if(qName.equals("item")){
-            isItem = false;
-            newsItems.add(item);
-        }
-        if(qName.equals("link")){
-            isLink = false;
-            if(isItem){
-                item.link = linkSB.toString();
-                linkSB = new StringBuilder();
-            }
+        switch(qName)
+        {
+            case "title":
+                isTitle = false;
+                break;
+            case "item":
+                isItem=false;
+                newsItems.add(item);
+                break;
+            case "link":
+                isLink = false;
+                if(isItem){
+                    item.link = linkSB.toString();
+                    linkSB = new StringBuilder();
+                }
+                break;
+            case "description":
+                isDescription = false;
+                if(isItem)
+                {
+                    String str = descSB.toString();
+                    Pattern pattern = Pattern.compile("https.*jpg");
+                    Matcher m = pattern.matcher(str);
+                    String imgurl = "";
+
+                    if(m.find())
+                    {
+                        imgurl=m.group(0);
+                        Log.d("NET",imgurl);
+                    }
+                    str = str.replaceAll("<img.*/>","");
+                    item.description = str;
+                    item.imgurl = imgurl;
+                }
+                break;
 
         }
+
+
 
     }
 
@@ -66,5 +104,12 @@ public class MyHandler extends DefaultHandler {
             Log.d("NET",new String(ch,start,length));
             linkSB.append(new String(ch,start,length));
         }
+        if(isDescription && isItem){
+
+            descSB.append(new String(ch,start,length));
+
+
+        }
+
     }
 }
