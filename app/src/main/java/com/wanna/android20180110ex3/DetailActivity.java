d7befd191fc334f0b5d4c18f5075a660396818f9
kay <ruby50333@yahoo.com.tw>
2018-01-11 13:43:06 +0800

    Initial commit

---
+++ app/src/main/java/com/wanna/android20180110ex3/DetailActivity.java
@@ -0,0 +1,23 @@
+package com.wanna.android20180110ex3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class DetailActivity extends AppCompatActivity {
+    WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Intent it = getIntent();
+        String link = it.getStringExtra("link");
+
+        wv = (WebView) findViewById(R.id.webView);
+        wv.setWebChromeClient(new WebChromeClient());
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl(link);
+    }
+}
---
+++ app/src/main/java/com/wanna/android20180110ex3/Mobile01NewsItem.java
@@ -0,0 +1,12 @@
+package com.wanna.android20180110ex3;
+
+/**
+ * Created by Student on 2018/1/11.
+ */
+
+public class Mobile01NewsItem {
+    public String title;
+    public String link;
+    public String imgurl;
+    public String description;
+}
